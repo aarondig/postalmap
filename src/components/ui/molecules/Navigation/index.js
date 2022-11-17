6d4f67f45ad99ccde2a5436cdaf90fd0e9e654fb
Aaron Diggdon <aarondiggdon@gmail.com>
2022-11-17 00:06:46 +0000

    Added react-spring hamburger animation

--- src/components/ui/molecules/Navigation/index.js
+++ src/components/ui/molecules/Navigation/index.js
@@ -9,7 +9,7 @@ function Navigation({current, audio, setAudio}) {
   const [active, toggleActive] = useState(false);
 
 const opacity = useSpring({
-  opacity: current > 0 ? 0 : 1
+  opacity: current <= 0 || active ? 1 : 0
 })
 // const wrapper = useSpring({
 //   height: active ? "100vh" : "0vh"
@@ -19,6 +19,19 @@ const background = useSpring({
   // width: active ? "150vw" : "0vh",
   clipPath: active ? `circle(2200px at 110% -10%)` : "circle(30px at 110vw -10%)"
 })
+// HAMBURGER ANIMATIONs
+const path1 = useSpring({
+  transform: active ? `translateX(9px) translateY(-4px) rotate(45deg)` : "translateY(0) translateX(0) rotate(0)"
+})
+const path2 = useSpring({
+  transform: active ? `translateY(8px) translateX(-8px) rotate(-45deg)` : "translateX(0) translateY(0) rotate(0)"
+})
+// const path1 = useSpring({
+//   transform: active ? `translate3d(-12px, -4px, 0) rotate(45deg)` : "translate3d(0, 0, 0) rotate(0)"
+// })
+// const path2 = useSpring({
+//   transform: active ? `translate3d(-4px, 12px, 0) rotate(-45deg)` : "translate3d(0, 0, 0) rotate(0)"
+// })
 
   return (
     <a.div id="navigation">
@@ -29,8 +42,8 @@ const background = useSpring({
         {/* <div className="menu" onClick={()=> setAudio(!audio)}> */}
         <div className="menu" onClick={()=> toggleActive(!active)}>
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M3 9H20" stroke="white" strokeWidth={.8}/>
-        <path d="M3 15H20" stroke="white" strokeWidth={.8}/>
+        <a.path style={path1} d="M3 9H20" stroke="white" strokeWidth={.8}/>
+        <a.path style={path2} d="M3 15H20" stroke="white" strokeWidth={.8}/>
         </svg>
 
         </div>
